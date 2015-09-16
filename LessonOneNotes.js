7417e42dd9bb9d1b179829a37766ba68c1e4a8cd
reneedudley <4reneedudley@gmail.com>
2015-09-16 10:53:12 -0500

    finished app

--- LessonOneNotes.js
+++ LessonOneNotes.js
@@ -1,1 +1,12 @@
-notes and suff for class 1 
+'strict mode';
+
+var prompt = require('prompt');
+prompt.start();
+prompt.get(['username', 'email'], function (err, result) {
+    // 
+    // Log the results. 
+    // 
+    console.log('Command-line input received:');
+    console.log('  username: ' + result.username);
+    console.log('  email: ' + result.email);})
+// hello <username>, your email is <email> 
--- PigLatin.js
+++ PigLatin.js
@@ -1,1 +1,24 @@
 'use strict';
+var prompt = require('prompt');
+
+
+function pigLatin(name) {
+	var vowels = ['a','e','i','o','u'];
+	if (vowels.indexOf(name[0]> -1)){
+		return name + 'way';
+		console.log('first name is a vowel')
+	}
+	else {
+		if (vowels.indexOf(name[1]> -1)){
+			return name.replace(name[0], '') + name[0] + 'ay';
+		}
+		else{
+			return name.replace(name[0]+name[1],"") + name[0]+name[1]+ 'ay';
+		}
+	}
+}
+prompt.start();
+prompt.get(['name'],function(error, result) {
+	console.log(pigLatin(result.name));
+});
+
