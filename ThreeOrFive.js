585be9e78b36341477b490ee1ea829890aa9fb19
reneedudley <4reneedudley@gmail.com>
2015-09-17 20:27:05 -0500

    done, bitch

--- ThreeOrFive.js
+++ ThreeOrFive.js
@@ -4,4 +4,12 @@ prompt.start();
 console.log("Let's get started!");
 prompt.get(['number'], function (error, result){
 	var num = parseInt(result['number']);
+	if (num % 3 === 0 && num % 5 ===0 ){
+		console.log(num + ' is divisible by 3 and 5!');
+	}
+	else if (num % 5 === 0){
+		console.log(num + ' is divisible by 5!');
+	}
+	else if (num % 3 === 0)
+		console.log(num + ' is divisible by 3');
 });
