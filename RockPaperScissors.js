7db42acc4c6b0c147ad39707ce8df30d2998485b
reneedudley <4reneedudley@gmail.com>
2015-09-18 11:22:39 -0500

    the game works, wee

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -3,32 +3,37 @@ var prompt = require('prompt');
 prompt.start();
 console.log('Lets get started');
 
+function compareHands(hand1, hand2){
+	if (hand1 === hand2){
+		return "its a tie";
+	}
+	if (hand1 === 'rock'){
+		if(hand2 === 'scissors'){
+			return "player 1 wins";
+		} 
+		else{
+			return "player 1 wins" 
+		}
+	}
+	if (hand1 === 'paper'){
+		if(hand2 === 'scissors'){
+			return "player 2 wins";
+		} 
+		else{
+			return "player 1 wins"
+		}
+	}
+	if (hand1 === 'scissors'){
+		if(hand2 === 'rock'){
+			return "player 1 wins" ;
+		} 
+		else{
+			return "player 2 wins";
+		}
+	}
+
+}
 prompt.get(['hand1', 'hand2'], function(error, result){
-	if (result['hand1'] === result['hand2']){
-		console.log("it's a tie");
-	}
-	if (result['hand1'] === 'rock'){
-		if(result['hand 2'] === 'scissors'){
-			console.log("player 1 wins");
-		} 
-		else{
-			console.log("player 1 wins");
-		}
-	}
-	if (result['hand1'] === 'paper'){
-		if(result['hand 2'] === 'scissors'){
-			console.log("player 2 wins");
-		} 
-		else{
-			console.log("player 1 wins");
-		}
-	}
-	if (result['hand1'] === 'rock'){
-		if(result['hand 2'] === 'scissors'){
-			console.log("player 1 wins");
-		} 
-		else{
-			console.log("player 2 wins");
-		}
-	}
+	console.log(compareHands(result['hand1'], result['hand2']));
 });
+
