e63913addf56c48881c9e4f05556615f1e91ffb4
reneedudley <4reneedudley@gmail.com>
2015-09-17 20:43:01 -0500

    logic, bitch

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,1 +1,34 @@
 'use strict';
+var prompt = require('prompt');
+prompt.start();
+console.log('Lets get started');
+
+prompt.get(['hand1', 'hand2'], function(error, result){
+	if (result['hand1'] === result['hand2']){
+		console.log("it's a tie");
+	}
+	if (result['hand1'] === 'rock'){
+		if(result['hand 2'] === 'scissors'){
+			console.log("player 1 wins");
+		} 
+		else{
+			console.log("player 1 wins");
+		}
+	}
+	if (result['hand1'] === 'paper'){
+		if(result['hand 2'] === 'scissors'){
+			console.log("player 2 wins");
+		} 
+		else{
+			console.log("player 1 wins");
+		}
+	}
+	if (result['hand1'] === 'rock'){
+		if(result['hand 2'] === 'scissors'){
+			console.log("player 1 wins");
+		} 
+		else{
+			console.log("player 2 wins");
+		}
+	}
+});
