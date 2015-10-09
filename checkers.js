26021d720c457d977908712bba65de355b915e92
reneedudley <4reneedudley@gmail.com>
2015-10-08 20:16:15 -0500

    the placecheckers fucking hates me

--- checkers.js
+++ checkers.js
@@ -1,4 +1,4 @@
-//components- checkers are responsible for it's color, it's position, if he's king 
+//components- checkers are responsible for it's color, it's position, if he's king, 12 checkers total 
 //game-responsible for starting, calling a winner, rules, ending, turn switching
 //board- responsible for placing checkers, creating checkers, removing checkers, locating a specific checker
 
@@ -16,21 +16,53 @@ function Checker(color,position){
 var grid= [];
 
 function Board(){
-	this.grid= function print(){
-			for (var i=0; i<9; i++){
-				grid.push([' ', ' ', ' ', ' ', ' ', ' ', ' ',' ']);
+	this.grid= [];
+	this.createGrid= function(){
+		for (var i=0; i<8; i++){
+				this.grid.push([' ', ' ', ' ', ' ', ' ', ' ', ' ',' ']);
 			}
-    		for (var i=0; i<9; i++){
-				console.log(newBoard[i].join(' '));
-			};}
+   		}
+	this.viewBoard= function(){
+		for(var i=0;i<this.grid.length;i++){
+			console.log(this.grid[i].join(' '));
+		}
+	}
 	this.checkers= [];
 	this.createCheckers= function(){
-		var whitePosition= [[0,1],[0,3],[0,5],[0,7]...]; //finish the damn board
-		var redPosition=[[5,0],[5,2]...];
-		for (var=i;i<12;i++){
+		var whitePosition= [
+			[0,0],[0,1],[0,3],[0,5],
+			[1,0],[1,2],[1,4],[1,6],
+			[2,1],[2,3],[2,5],[2,7]]; 
+		var redPosition=[
+			[5,0],[5,2],[5,4],[5,6],
+			[6,1],[6,3],[6,5],[6,7],
+			[7,0],[7,2],[7,4],[7,6]];
+		for (var i=0;i<12;i++){
 			this.checkers.push(new Checker("white",whitePosition[i]));
 			this.checkers.push(new Checker("red",redPosition[i]));
 		}
+		for (var i=0;i<12;i++){
+			var whitechecker = new Checker("white",whitePosition[i]);
+			this.checkers.push(whitechecker);
+			var redcheckers = new Checker("white",redPosition[i]);
+			this.checkers.push(redcheckers);
+		}
 	}
+	this.placeCheckers= function(){
+		for (var i=0; i<this.checkers.length;i++){
+			var checker= this.checkers[i]
+			var checkerPostion= checker.position //[0,1]
+			this.grid [ [checkerPostion[0]] [checkerPostion[i] ]= checker.color
+		}
+	
 
-}
+var Board = new Board();
+console.log(Board);
+Board.createGrid();
+Board.viewBoard();
+Board.createCheckers();
+console.log(Board);
+Board.placeCheckers();
+console.log(Board);
+
+
