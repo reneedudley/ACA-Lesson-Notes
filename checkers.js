11ad5a174877598cc2ef7aedba2650b583268836
reneedudley <4reneedudley@gmail.com>
2015-10-08 19:07:05 -0500

    planning and shit

---
+++ checkers.js
@@ -0,0 +1,9 @@
+//components- checkers are responsible for it's color, it's position, if he's king 
+//game-responsible for starting, calling a winner, rules, ending, turn switching
+//board- responsible for placing checkers, creating checkers, removing checkers, locating a specific checker
+
+'use script';
+
+var colors= require('colors/safe');
+var prompt= require('prompt');
+prompt.start();
